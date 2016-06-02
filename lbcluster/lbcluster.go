dd8f2fbfa06332b08af52b21b259b373475cddf4
Ignacio Reguero <Ignacio.Reguero@cern.ch>
2016-06-02 21:38:24 +0200

    first concurrent implementation of the snmp queries

--- lbcluster/lbcluster.go
+++ lbcluster/lbcluster.go
@@ -6,15 +6,18 @@ import (
 	//"github.com/tiebingzhang/wapsnmp"
 	"github.com/k-sone/snmpgo"
 	"io/ioutil"
+	"math/rand"
 	"net"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
 const WorstValue int = 99999
+const TIMEOUT int = 5
 const OID string = ".1.3.6.1.4.1.96.255.1"
 
 type LBCluster struct {
@@ -40,6 +43,12 @@ type Params struct {
 	Statistics       string
 }
 
+type RetSnmp struct {
+	Metric int
+	Host   string
+	Log    string
+}
+
 func (self LBCluster) Time_to_refresh() bool {
 	if self.Time_of_last_evaluation.IsZero() {
 		return true
@@ -54,11 +63,21 @@ func (self LBCluster) find_best_hosts() {
 }
 
 func (self LBCluster) Evaluate_hosts() {
+	var wg sync.WaitGroup
+	result := make(chan RetSnmp, 200)
 	for h := range self.Host_metric_table {
 		fmt.Println("contacting cluster: " + self.Cluster_name + " node: " + h)
-		metric, host, logm := self.snmp_req(h)
-		fmt.Printf("%v\n%v %v\n", logm, host, metric)
+		wg.Add(1)
+		go self.snmp_req(h, &wg, result)
 	}
+	for range self.Host_metric_table {
+		time.Sleep(1 * time.Millisecond)
+		select {
+		case metrichostlog := <-result:
+			fmt.Printf("%v\n%v %v\n", metrichostlog.Log, metrichostlog.Host, metrichostlog.Metric)
+		}
+	}
+	wg.Wait()
 }
 
 func TimeoutDialer(cTimeout time.Duration, rwTimeout time.Duration) func(net, addr string) (c net.Conn, err error) {
@@ -81,12 +100,14 @@ func NewTimeoutClient(connectTimeout time.Duration, readWriteTimeout time.Durati
 	}
 }
 
-func (self LBCluster) snmp_req(host string) (int, string, string) {
+func (self LBCluster) snmp_req(host string, wg *sync.WaitGroup, result chan<- RetSnmp) {
+	defer wg.Done()
+	time.Sleep(time.Duration(rand.Int31n(5000)) * time.Millisecond)
 	metric := -100
 	logmessage := ""
 	if self.Parameters.Metric == "cmsweb" {
-		connectTimeout := (20 * time.Second)
-		readWriteTimeout := (40 * time.Second)
+		connectTimeout := (10 * time.Second)
+		readWriteTimeout := (20 * time.Second)
 		httpClient := NewTimeoutClient(connectTimeout, readWriteTimeout)
 		response, err := httpClient.Get("http://woger-direct.cern.ch:9098/roger/v1/state/" + host)
 		if err != nil {
@@ -101,13 +122,17 @@ func (self LBCluster) snmp_req(host string) (int, string, string) {
 			}
 			var dat map[string]interface{}
 			if err := json.Unmarshal([]byte(contents), &dat); err != nil {
+				fmt.Println(host)
+				fmt.Println(response.Body)
+				fmt.Println(contents)
 				panic(err)
 			}
 			if str, ok := dat["appstate"].(string); ok {
 				if str != "production" {
 					metric = -99
 					logmessage = fmt.Sprintf("cluster: %s node: %s - %s - setting reply %v", self.Cluster_name, host, str, metric)
-					return metric, host, logmessage
+					result <- RetSnmp{metric, host, logmessage}
+					return
 				}
 			} else {
 				fmt.Printf("dat[\"appstate\"] not a string for node %s", host)
@@ -120,16 +145,18 @@ func (self LBCluster) snmp_req(host string) (int, string, string) {
 	snmp, err := snmpgo.NewSNMP(snmpgo.SNMPArguments{
 		Version:       snmpgo.V3,
 		Address:       host + ":161",
-		Retries:       1,
+		Retries:       2,
 		UserName:      self.Loadbalancing_username,
 		SecurityLevel: snmpgo.AuthNoPriv,
 		AuthProtocol:  snmpgo.Md5,
 		AuthPassword:  self.Loadbalancing_password,
+		Timeout:       time.Duration(TIMEOUT) * time.Second,
 	})
 	if err != nil {
 		// Failed to create snmpgo.SNMP object
 		fmt.Println(err)
-		return metric, host, fmt.Sprintf("%v\n", err)
+		result <- RetSnmp{metric, host, fmt.Sprintf("%v\n", err)}
+		return
 	}
 
 	oids, err := snmpgo.NewOids([]string{
@@ -138,13 +165,14 @@ func (self LBCluster) snmp_req(host string) (int, string, string) {
 	if err != nil {
 		// Failed to parse Oids
 		fmt.Println(err)
-		return metric, host, fmt.Sprintf("%v\n", err)
+		result <- RetSnmp{metric, host, fmt.Sprintf("%v\n", err)}
+		return
 	}
-
 	if err = snmp.Open(); err != nil {
 		// Failed to open connection
 		fmt.Println(err)
-		return metric, host, fmt.Sprintf("snmp open failed with %v\n", err)
+		result <- RetSnmp{metric, host, fmt.Sprintf("snmp open failed with %v\n", err)}
+		return
 	}
 	defer snmp.Close()
 
@@ -152,39 +180,38 @@ func (self LBCluster) snmp_req(host string) (int, string, string) {
 	if err != nil {
 		// Failed to request
 		fmt.Println(err)
-		return metric, host, fmt.Sprintf("snmp get failed with %v\n", err)
+		result <- RetSnmp{metric, host, fmt.Sprintf("%v: snmp get failed with %v\n", host, err)}
+		return
 	}
 	if pdu.ErrorStatus() != snmpgo.NoError {
 		// Received an error from the agent
 		fmt.Println(pdu.ErrorStatus(), pdu.ErrorIndex())
 	}
 
-	// get VarBind list
-	fmt.Println(pdu.VarBinds())
-
 	// select a VarBind
 	Varbind := pdu.VarBinds().MatchOid(oids[0])
-	fmt.Println(Varbind.Variable)
-	fmt.Println(pdu.VarBinds().MatchOid(oids[0]))
-	if pdu.VarBinds().MatchOid(oids[0]).Variable.Type() == "Integer" {
-		metricstr := pdu.VarBinds().MatchOid(oids[0]).Variable.String()
+	if Varbind.Variable.Type() == "Integer" {
+		metricstr := Varbind.Variable.String()
 		if metric, err = strconv.Atoi(metricstr); err != nil {
 			fmt.Println(err)
-			return metric, host, fmt.Sprintf("%v\n", err)
+			result <- RetSnmp{metric, host, fmt.Sprintf("%v\n", err)}
+			return
 		}
-	} else if pdu.VarBinds().MatchOid(oids[0]).Variable.Type() == "OctetString" {
-		cskvpair := pdu.VarBinds().MatchOid(oids[0]).Variable.String()
+	} else if Varbind.Variable.Type() == "OctetString" {
+		cskvpair := Varbind.Variable.String()
 		kvpair := strings.Split(cskvpair, ",")
 		for _, kv := range kvpair {
 			cm := strings.Split(kv, "=")
 			if cm[0] == self.Cluster_name {
 				if metric, err = strconv.Atoi(cm[1]); err != nil {
 					fmt.Println(err)
-					return metric, host, fmt.Sprintf("%v\n", err)
+					result <- RetSnmp{metric, host, fmt.Sprintf("%v\n", err)}
+					return
 				}
 			}
 		}
 	}
 
-	return metric, host, logmessage
+	result <- RetSnmp{metric, host, logmessage}
+	return
 }
