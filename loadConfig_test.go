3d533b6187f6e8d6fce5dde55475db282637eda5
Ignacio Reguero <Ignacio.Reguero@cern.ch>
2017-05-05 21:55:05 +0200

    config file packaged within the test file

--- loadConfig_test.go
+++ loadConfig_test.go
@@ -3,11 +3,56 @@ package main
 import (
 	"github.com/cernops/golbd/lbcluster"
 	//"sync"
+	"io/ioutil"
 	"reflect"
+	"syscall"
 	"testing"
 )
 
 func TestLoadConfig(t *testing.T) {
+	f, err := ioutil.TempFile("", "testloadconfig")
+	if err != nil {
+		panic(err)
+	}
+	defer syscall.Unlink(f.Name())
+	ioutil.WriteFile(f.Name(), []byte(`#
+# Who is the primary master to upload the data ?
+#  - fully qualified DNS name
+#
+master = lbdxyz.cern.ch
+
+#
+# Heartbeat details
+#
+heartbeat_path = /work/go/src/github.com/cernops/golbd
+heartbeat_file = heartbeat
+
+#
+# TSIG HMAC-MD5 algorithm keys for DNS access
+#
+tsig_key_prefix = abcd-
+tsig_internal_key = xxx123==
+tsig_external_key = yyy123==
+
+#
+# SNMPv3 password for 'loadbalancing' user
+#
+snmpd_password = zzz123
+
+#
+# Which node manages information in DNS servers ?
+#
+dns_manager = 111.111.0.111
+
+#
+# Clusters definitions
+#
+parameters test01.cern.ch = behaviour#mindless best_hosts#2 external#yes metric#cmsfrontier polling_interval#6 statistics#long
+parameters test02.cern.ch = behaviour#mindless best_hosts#10 external#no metric#cmsfrontier polling_interval#6 statistics#long
+
+clusters test01.cern.ch = lxplus142.cern.ch lxplus177.cern.ch
+clusters test02.cern.ch = lxplus013.cern.ch lxplus038.cern.ch lxplus025.cern.ch
+`), 0644)
 	lg := lbcluster.Log{Syslog: false, Stdout: true, Debugflag: false}
 	expected := Config{Master: "lbdxyz.cern.ch",
 		HeartbeatFile: "heartbeat",
@@ -23,7 +68,7 @@ func TestLoadConfig(t *testing.T) {
 		Parameters: map[string]lbcluster.Params{"test01.cern.ch": lbcluster.Params{Behaviour: "mindless", Best_hosts: 2, External: true, Metric: "cmsfrontier", Polling_interval: 6, Statistics: "long"},
 			"test02.cern.ch": lbcluster.Params{Behaviour: "mindless", Best_hosts: 10, External: false, Metric: "cmsfrontier", Polling_interval: 6, Statistics: "long"}}}
 
-	config, e := loadConfig("./loadConfigtst.conf", lg)
+	config, e := loadConfig(f.Name(), lg)
 	if e != nil {
 		t.Errorf("loadConfig Error: %v", e.Error())
 	} else {
