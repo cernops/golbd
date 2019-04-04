7817c8cfc241e9f185ef2d5272e60ca01af09156
Pablo Saiz <psaiz@cern.ch>
2019-04-04 13:58:15 +0200

    Continue with the dns update if the lookup of hosts fails

--- lbcluster/lbcluster_landb.go
+++ lbcluster/lbcluster_landb.go
@@ -13,47 +13,47 @@ import (
 
 /* This is the only public function here. It retrieves the status of the dns,
 and then updates it with the new hosts */
-func (self *LBCluster) Refresh_dns(dnsManager, keyPrefix, internalKey, externalKey string, hosts_to_check map[string]lbhost.LBHost) {
+func (lbc *LBCluster) Refresh_dns(dnsManager, keyPrefix, internalKey, externalKey string, hosts_to_check map[string]lbhost.LBHost) {
 
-	e := self.get_state_dns(dnsManager)
+	e := lbc.get_state_dns(dnsManager)
 	if e != nil {
-		self.Write_to_log("WARNING", fmt.Sprintf("Get_state_dns Error: %v", e.Error()))
+		lbc.Write_to_log("WARNING", fmt.Sprintf("Get_state_dns Error: %v", e.Error()))
 		return
 	}
-	e = self.update_dns(keyPrefix+"internal.", internalKey, dnsManager, hosts_to_check)
+	e = lbc.update_dns(keyPrefix+"internal.", internalKey, dnsManager, hosts_to_check)
 	if e != nil {
-		self.Write_to_log("WARNING", fmt.Sprintf("Internal Update_dns Error: %v", e.Error()))
+		lbc.Write_to_log("WARNING", fmt.Sprintf("Internal Update_dns Error: %v", e.Error()))
 	}
-	if self.externally_visible() {
-		e = self.update_dns(keyPrefix+"external.", externalKey, dnsManager, hosts_to_check)
+	if lbc.externallyVisible() {
+		e = lbc.update_dns(keyPrefix+"external.", externalKey, dnsManager, hosts_to_check)
 		if e != nil {
-			self.Write_to_log("WARNING", fmt.Sprintf("External Update_dns Error: %v", e.Error()))
+			lbc.Write_to_log("WARNING", fmt.Sprintf("External Update_dns Error: %v", e.Error()))
 		}
 	}
 }
 
 //Internal functions
-func (self *LBCluster) externally_visible() bool {
-	return self.Parameters.External
+func (lbc *LBCluster) externallyVisible() bool {
+	return lbc.Parameters.External
 }
 
-func (self *LBCluster) update_dns(keyName, tsigKey, dnsManager string, hosts_to_check map[string]lbhost.LBHost) error {
-	pbhDns := strings.Join(self.Previous_best_hosts_dns, " ")
-	cbh := strings.Join(self.Current_best_hosts, " ")
+func (lbc *LBCluster) update_dns(keyName, tsigKey, dnsManager string, hosts_to_check map[string]lbhost.LBHost) error {
+	pbhDns := strings.Join(lbc.Previous_best_hosts_dns, " ")
+	cbh := strings.Join(lbc.Current_best_hosts, " ")
 	if pbhDns == cbh {
-		self.Write_to_log("INFO", fmt.Sprintf("DNS not update keyName %v cbh == pbhDns == %v", keyName, cbh))
+		lbc.Write_to_log("INFO", fmt.Sprintf("DNS not update keyName %v cbh == pbhDns == %v", keyName, cbh))
 		return nil
 	}
-	self.Write_to_log("INFO", fmt.Sprintf("Updating the DNS with %v (previous state was %v)", cbh, pbhDns))
-	cluster_name := self.Cluster_name
+	lbc.Write_to_log("INFO", fmt.Sprintf("Updating the DNS with %v (previous state was %v)", cbh, pbhDns))
+	cluster_name := lbc.Cluster_name
 	if !strings.HasSuffix(cluster_name, ".cern.ch") {
 		cluster_name = cluster_name + ".cern.ch"
 	}
 	ttl := "60"
-	if self.Parameters.Ttl > 60 {
-		ttl = fmt.Sprintf("%d", self.Parameters.Ttl)
+	if lbc.Parameters.Ttl > 60 {
+		ttl = fmt.Sprintf("%d", lbc.Parameters.Ttl)
 	}
-	//best_hosts_len := len(self.Current_best_hosts)
+	//best_hosts_len := len(lbc.Current_best_hosts)
 	m := new(dns.Msg)
 	m.SetUpdate(cluster_name + ".")
 	m.Id = 1234
@@ -62,11 +62,11 @@ func (self *LBCluster) update_dns(keyName, tsigKey, dnsManager string, hosts_to_
 	m.RemoveRRset([]dns.RR{rr_removeA})
 	m.RemoveRRset([]dns.RR{rr_removeAAAA})
 
-	for _, hostname := range self.Current_best_hosts {
+	for _, hostname := range lbc.Current_best_hosts {
 		my_host := hosts_to_check[hostname]
 		ips, err := my_host.Get_working_IPs()
 		if err != nil {
-			self.Write_to_log("WARNING", fmt.Sprintf("LookupIP: %v has incorrect or missing IP address (%v)", hostname, err))
+			lbc.Write_to_log("WARNING", fmt.Sprintf("LookupIP: %v has incorrect or missing IP address (%v)", hostname, err))
 			continue
 		}
 		for _, ip := range ips {
@@ -79,20 +79,20 @@ func (self *LBCluster) update_dns(keyName, tsigKey, dnsManager string, hosts_to_
 			m.Insert([]dns.RR{rr_insert})
 		}
 	}
-	self.Write_to_log("INFO", fmt.Sprintf("WE WOULD UPDATE THE DNS WITH THE IPS %v", m))
+	lbc.Write_to_log("INFO", fmt.Sprintf("WE WOULD UPDATE THE DNS WITH THE IPS %v", m))
 	c := new(dns.Client)
 	m.SetTsig(keyName, dns.HmacMD5, 300, time.Now().Unix())
 	c.TsigSecret = map[string]string{keyName: tsigKey}
 	_, _, err := c.Exchange(m, dnsManager+":53")
 	if err != nil {
-		self.Write_to_log("ERROR", fmt.Sprintf("DNS update failed with (%v)", err))
+		lbc.Write_to_log("ERROR", fmt.Sprintf("DNS update failed with (%v)", err))
 	}
-	self.Write_to_log("INFO", fmt.Sprintf("DNS update with keyName %v", keyName))
+	lbc.Write_to_log("INFO", fmt.Sprintf("DNS update with keyName %v", keyName))
 	return err
 }
 
-func (self *LBCluster) get_state_dns(dnsManager string) error {
-	cluster_name := self.Cluster_name
+func (lbc *LBCluster) get_state_dns(dnsManager string) error {
+	cluster_name := lbc.Cluster_name
 	if !strings.HasSuffix(cluster_name, ".cern.ch") {
 		cluster_name = cluster_name + ".cern.ch"
 	}
@@ -101,29 +101,29 @@ func (self *LBCluster) get_state_dns(dnsManager string) error {
 	m.SetQuestion(cluster_name+".", dns.TypeA)
 	in, err := dns.Exchange(m, dnsManager+":53")
 	if err != nil {
-		self.Write_to_log("ERROR", fmt.Sprintf("Error getting the ipv4 state of dns: %v", err))
+		lbc.Write_to_log("ERROR", fmt.Sprintf("Error getting the ipv4 state of dns: %v", err))
 		return err
 	}
 
 	var ips []net.IP
 	for _, a := range in.Answer {
 		if t, ok := a.(*dns.A); ok {
-			self.Slog.Debug(fmt.Sprintf("%v", t))
-			self.Slog.Debug(fmt.Sprintf("%v", t.A))
+			lbc.Slog.Debug(fmt.Sprintf("%v", t))
+			lbc.Slog.Debug(fmt.Sprintf("%v", t.A))
 			ips = append(ips, t.A)
 		}
 	}
 	m.SetQuestion(cluster_name+".", dns.TypeAAAA)
 	in, err = dns.Exchange(m, dnsManager+":53")
 	if err != nil {
-		self.Write_to_log("ERROR", fmt.Sprintf("Error getting the ipv6 state of dns: %v", err))
+		lbc.Write_to_log("ERROR", fmt.Sprintf("Error getting the ipv6 state of dns: %v", err))
 		return err
 	}
 
 	for _, a := range in.Answer {
 		if t, ok := a.(*dns.AAAA); ok {
-			self.Slog.Debug(fmt.Sprintf("%v", t))
-			self.Slog.Debug(fmt.Sprintf("%v", t.AAAA))
+			lbc.Slog.Debug(fmt.Sprintf("%v", t))
+			lbc.Slog.Debug(fmt.Sprintf("%v", t.AAAA))
 			ips = append(ips, t.AAAA)
 		}
 	}
@@ -141,8 +141,13 @@ func (self *LBCluster) get_state_dns(dnsManager string) error {
 	for _, ip := range ips {
 		names, err := net.LookupAddr(ip.String())
 		if err != nil {
-			self.Write_to_log("ERROR", fmt.Sprintf("Error getting the state of the dns %v", err))
-			return err
+			lbc.Write_to_log("ERROR", fmt.Sprintf("Error getting the state of the dns %v", err))
+			if _, ok := err.(*net.DNSError); ok {
+				lbc.Write_to_log("INFO", "The host does not exist anymore... let's continue")
+			} else {
+				lbc.Write_to_log("INFO", "Different error")
+				return err
+			}
 		}
 		if len(names) > 0 {
 			if len(names) == 1 {
@@ -150,7 +155,7 @@ func (self *LBCluster) get_state_dns(dnsManager string) error {
 			} else {
 				name, err = net.LookupCNAME(names[0])
 				if err != nil {
-					self.Write_to_log("ERROR", fmt.Sprintf("Error getting the state of the dns %v", err))
+					lbc.Write_to_log("ERROR", fmt.Sprintf("Error getting the state of the dns %v", err))
 					return err
 				}
 				name = strings.TrimRight(name, ".")
@@ -164,19 +169,19 @@ func (self *LBCluster) get_state_dns(dnsManager string) error {
 	// The order in which the DNS returns the hosts does not seem to depend on the order in which
 	// they have been set. Let's sort them to avoid unnecessary udpates
 	sort.Strings(host_list)
-	self.Previous_best_hosts_dns = host_list
+	lbc.Previous_best_hosts_dns = host_list
 
-	pbhDns := strings.Join(self.Previous_best_hosts_dns, " ")
-	pbh := strings.Join(self.Previous_best_hosts, " ")
-	cbh := strings.Join(self.Current_best_hosts, " ")
+	pbhDns := strings.Join(lbc.Previous_best_hosts_dns, " ")
+	pbh := strings.Join(lbc.Previous_best_hosts, " ")
+	cbh := strings.Join(lbc.Current_best_hosts, " ")
 	if pbh != "unknown" {
 		if pbh != pbhDns {
-			self.Write_to_log("WARNING", "Prev DNS state "+pbhDns+" - Prev local state  "+pbh+" differ")
+			lbc.Write_to_log("WARNING", "Prev DNS state "+pbhDns+" - Prev local state  "+pbh+" differ")
 		}
 	}
 	if cbh == "unknown" {
-		self.Write_to_log("WARNING", "Current best hosts are unknown - Taking Previous DNS state  "+pbhDns)
-		self.Current_best_hosts = self.Previous_best_hosts_dns
+		lbc.Write_to_log("WARNING", "Current best hosts are unknown - Taking Previous DNS state  "+pbhDns)
+		lbc.Current_best_hosts = lbc.Previous_best_hosts_dns
 	}
 	return err
 }
